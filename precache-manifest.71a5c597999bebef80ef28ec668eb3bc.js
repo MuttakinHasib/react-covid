0fa84cb2c3ecde6e57b19bb112107717e070d1d9
MuttakinHasib <hasibmolla28@gmail.com>
2020-04-07 16:47:28 -0700

    Updates

---
+++ precache-manifest.71a5c597999bebef80ef28ec668eb3bc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46d58b9b0b46c427b7751a84385fecb5",
+    "url": "/react-covid/index.html"
+  },
+  {
+    "revision": "7e90714f8145add7dd17",
+    "url": "/react-covid/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "f3657bd41176f64b3731",
+    "url": "/react-covid/static/css/main.68e66fd3.chunk.css"
+  },
+  {
+    "revision": "7e90714f8145add7dd17",
+    "url": "/react-covid/static/js/2.7fd801b9.chunk.js"
+  },
+  {
+    "revision": "4603884de8b017b7782e1d479ad0aac1",
+    "url": "/react-covid/static/js/2.7fd801b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3657bd41176f64b3731",
+    "url": "/react-covid/static/js/main.76771e49.chunk.js"
+  },
+  {
+    "revision": "d5af102cbecb2d153f4c",
+    "url": "/react-covid/static/js/runtime-main.9d9a57c7.js"
+  },
+  {
+    "revision": "feea5fb1c98ed41237fa5d7d7be3c822",
+    "url": "/react-covid/static/media/cases.feea5fb1.png"
+  },
+  {
+    "revision": "b379f9d1c1c8700841b2439d529f9919",
+    "url": "/react-covid/static/media/deaths-large.b379f9d1.png"
+  },
+  {
+    "revision": "dd8ede4e66d9fed9d3bfbb91f254f884",
+    "url": "/react-covid/static/media/land.dd8ede4e.png"
+  },
+  {
+    "revision": "66525ad43d232f75d95044619962bb3b",
+    "url": "/react-covid/static/media/recovered.66525ad4.png"
+  }
+]);
