94a470c602f42d4c78b110c8ac57a588c2224e1c
MuttakinHasib <hasibmolla28@gmail.com>
2020-04-07 17:54:59 -0700

    fixed an issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <footer>
       <div className='container'>
         <p className='lead text-center p-4'>
           Made with <i className='fas heart fa-heart'></i> by{' '}
-          <Link
-            className='text-primary'
-            href='https://facebook.com/hasibmolla28/'
-            target='_blank'
-          >
+          <a className='text-primary' href='https://facebook.com/hasibmolla28/'>
             Hasib
-          </Link>
+          </a>
         </p>
       </div>
     </footer>
