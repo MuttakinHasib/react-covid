ab8031d8d855b7f27ea0b2d0d9688a6ef1c1447c
MuttakinHasib <hasibmolla28@gmail.com>
2020-04-07 17:37:11 -0700

    fixed an issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,11 @@ const Footer = () => {
       <div className='container'>
         <p className='lead text-center p-4'>
           Made with <i className='fas heart fa-heart'></i> by{' '}
-          <Link href='https://facebook.com/hasibmolla28/' target='_blank'>
+          <Link
+            className='text-primary'
+            href='https://facebook.com/hasibmolla28/'
+            target='_blank'
+          >
             Hasib
           </Link>
         </p>
